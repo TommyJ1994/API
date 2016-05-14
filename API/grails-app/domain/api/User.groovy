14220d0c852bd77eed5b704562248151e8dd739f
Tom Jackman <tom.dls@hotmail.com>
2016-05-14 11:15:37 +0100

    Added user fields.

--- API/grails-app/domain/api/User.groovy
+++ API/grails-app/domain/api/User.groovy
@@ -2,6 +2,27 @@ package api
 
 class User {
 
+    String gender
+    def name = [:]
+    def location = [:]
+    String email
+    String username
+    String password
+    String salt
+    String md5
+    String sha1
+    String sha256
+    Date registered
+    Date dob
+    String phone
+    String cell
+    String PPS
+    def picture = [:]
+
     static constraints = {
     }
+
+    def beforeValidate() {
+       // convert dates
+   }
 }
