90d9f1ee38a3159332ab80304417af0f32b6a0d8
Tom Jackman <tom.dls@hotmail.com>
2016-05-15 10:30:06 +0100

    Implemented api update method.

--- API/grails-app/controllers/api/UserController.groovy
+++ API/grails-app/controllers/api/UserController.groovy
@@ -25,12 +25,13 @@ class UserController {
     /**
     * This method returns data for a user found using the email address sent with the GET request.
     * @HttpMethod GET
+    * @params id = the email address of the user
     */
     def show() {
       String email = params.id
       def user = [user: User.findByEmail(email)]
       if(user == null) {
-          render status:404
+          render status:NOT_FOUND
       }
       else {
           render user as JSON
@@ -48,7 +49,7 @@ class UserController {
       def location = [street: data.location.street, city: data.location.city, state: data.location.state, zip: data.location.zip]
       def picture = [large: data.picture.large, medium: data.picture.medium, thumbnail: data.picture.thumbnail]
 
-      def user = new User(gender: 'male',
+      def user = new User(gender: data.gender,
   			name: name,
         location: location,
         email: data.email,
@@ -66,15 +67,64 @@ class UserController {
         picture: picture
         )
 
-        user.validate()
+      user.validate()
     		if (user.hasErrors()) {
     			respond status: NOT_ACCEPTABLE
     			println user.errors
     			return
     		}
 
+      user.save(flush:true)
+      respond status: CREATED
+    }
+
+    /**
+    * This method updates the data for a user found using the email address sent with the the PUT request.
+    * @HttpMethod PUT
+    * @params id = the email address of the user
+    */
+    def update(){
+      String email = params.id
+      def user = User.findByEmail(email)
+
+      if(user == null) {
+          render status:404
+      }
+      else {
+        def data = request.JSON.user
+
+        def name = [title: data.name.title, first: data.name.first, last: data.name.last]
+        def location = [street: data.location.street, city: data.location.city, state: data.location.state, zip: data.location.zip]
+        def picture = [large: data.picture.large, medium: data.picture.medium, thumbnail: data.picture.thumbnail]
+
+          user.gender = data.gender
+          user.name = name
+          user.location = location
+          user.email = data.email
+          user.username = data.username
+          user.password = data.password
+          user.salt = data.salt
+          user.md5 = data.md5
+          user.sha1 = data.sha1
+          user.sha256 = data.sha256
+          user.registered = data.registered
+          user.dob = data.dob
+          user.phone = data.phone
+          user.cell = data.cell
+          user.PPS = data.PPS
+          user.picture = picture
+
+        user.validate()
+          if (user.hasErrors()) {
+            respond status: NOT_ACCEPTABLE
+            println user.errors
+            return
+          }
+
         user.save(flush:true)
-        respond status: CREATED
+        respond status: OK
+      }
+
     }
 
 }
